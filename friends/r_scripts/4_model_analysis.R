88205c59a246cf3f7291b3ce542ec0ec94b34d99
Alexander Oyebanji <ako2816@ads.northwestern.edu>
2024-05-12 22:45:47 -0500

    Memo started

--- friends/r_scripts/4_model_analysis.R
+++ friends/r_scripts/4_model_analysis.R
@@ -29,18 +29,14 @@ nb_metrics
 nb_pred <-
   friends_test |> 
   select(season) |> 
-  bind_cols(predict(nb_res, friends_test))
+  bind_cols(predict(final_nb_train, friends_test))
 nb_pred
 
-nb_pred |> 
-  roc_curve(truth = product, .pred_class) |> 
-  autoplot() |> 
-  labs(
-    title = "ROC curve for Friend's Show Dialogue"
-  )
-
-conf_mat_resampled(nb_res, tidy = FALSE) |> 
+nb_conf_mat <- conf_mat_resampled(nb_res, tidy = FALSE) |> 
   autoplot(type = "heatmap")
+
+nb_accuracy <- accuracy(nb_pred, truth = season, .pred_class) |> 
+  mutate(model = "naive bayes")
 
 # Assessing null trained model ----
 null_metrics <- collect_metrics(null_res)
@@ -58,14 +54,20 @@ null_pred <-
   bind_cols(predict(final_null_train, friends_test))
 null_pred
 
-final_null_accuracy <- accuracy(null_pred, truth = season, .pred_class) |> 
+final_accuracy <- accuracy(null_pred, truth = season, .pred_class) |> 
+  mutate(model = "null") |> 
+  full_join(nb_accuracy) |> 
+  arrange(.estimate) |> 
   knitr::kable() |> 
   kableExtra::kable_paper()
+final_null_accuracy
 
-conf_mat_null <- conf_mat_resampled(nb_res, tidy = FALSE) |> 
+conf_mat_null <- conf_mat_resampled(null_res, tidy = FALSE) |> 
   autoplot(type = "heatmap")
+conf_mat_null
 
 # saving stuff
-save(final_null_accuracy, file = here("friends/figures/final_null_accuracy.rda"))
+save(final_null_accuracy, file = here("friends/figures/final_accuracy.rda"))
 save(conf_mat_null, file = here("friends/figures/conf_mat_null.rda"))
 save(training_metrics, file = here("friends/figures/training_metrics.rda"))
+save(nb_conf_mat, file = here("friends/figures/nb_conf_mat.rda"))
